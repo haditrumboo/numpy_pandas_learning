f61898921e5a77baf09360fe6dc8d63d34f07063
Hadi <haditrumboo@gmail.com>
2026-09-03 16:17:08 +0530

    Practice Pandas data filtering, grouping, and cleaning

--- pandas/file.py
+++ pandas/file.py
@@ -15,4 +15,12 @@ df = pd.read_csv("files/sales.csv")
 #     (df["quantity"] > 5) &
 #     (df["city"] == "Mumbai")
 # ])
-print(df[df["product"].isin(["Laptop", "Keyboard"])])
+# print(df[df["product"].isin(["Laptop", "Keyboard"])])
+# print(df.sort_values("price", ascending=False))
+# print(df.groupby("city")["quantity"].sum())
+# print(df.groupby("product")["price"].mean())
+# print(df[df.isnull().any(axis=1)])
+df["salesperson"] = df["salesperson"].fillna("adil")
+df["salesperson"] =df["salesperson"].str.upper()
+print(df)
+
